f8f32204ff4777e38418d691c6eefc06caeb9175
Matthew Becker <mateo62009mp@gmail.com>
2024-12-02 21:42:03 -0300

    Delete libs directory

--- libs/response.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Response {
-    public function send($message, $statusCode = 200) {
-        http_response_code($statusCode);
-        echo json_encode(['message' => $message]);
-    }
-}
